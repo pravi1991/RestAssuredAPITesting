191152d551ef1f185a96af41aaf48d5b91856f29
praveen <praveen@devopsnexus.com>
2019-12-23 17:31:18 +0530

    test

--- src/test/java/TC001_GET_Request.java
+++ src/test/java/TC001_GET_Request.java
@@ -17,7 +17,7 @@ public class TC001_GET_Request {
  void getweatherDetails()
  {
   //Specify base URI
-  RestAssured.baseURI="http://restapi.demoqa.com/utilities/weathr/city";
+  RestAssured.baseURI="google.com";
   
   //Request object
   RequestSpecification httpRequest=RestAssured.given();
